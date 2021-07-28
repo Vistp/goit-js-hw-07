1cdb233cbb331030fbd0dbf13d52290f6438bbf0
Irina Varushkina <73347907+Vistp@users.noreply.github.com>
2021-07-28 22:25:42 +0500

    Правки по замечаниям задачи 1, 8

--- js/task-01.js
+++ js/task-01.js
@@ -3,15 +3,11 @@ console.log(`В списке ${categoriesEl.length} категории.`);
 
 const itemTitleEl = document.querySelectorAll('h2');
 
-const animalsEl = document.querySelectorAll('.item__animals>li');
-const productsEl = document.querySelectorAll('.item__products>li');
-const technologyEl = document.querySelectorAll('.item__technology>li');
-
-const itemElements = [animalsEl, productsEl, technologyEl];
+const itemListEl = document.querySelectorAll('.item > ul');
 
 itemTitleEl.forEach((element,index) => {
     console.log(`Категория: ${element.textContent}`),
-    console.log(`Количество элементов: ${itemElements[index].length}`)
+    console.log(`Количество элементов: ${itemListEl[index].children.length}`)
 }
 )
 
--- js/task-08.js
+++ js/task-08.js
@@ -1,13 +1,40 @@
+const counter = {
+    value: 0,
+    increment() {
+        this.value += 1;
+    },
+    decrement() {
+        this.value = 0;
+    },
+}
+
 const renderBtn = document.querySelector('button[data-action="render"]');
 const destroyBtn = document.querySelector('button[data-action="destroy"]');
 const valueEl = document.querySelector('input[type="number"]');
 const boxesEl = document.querySelector('#boxes');
 
+// функции для подсчета количества созданных дивов
+let total = 0;
+const countTotal = (amount) => {
+    amount = valueEl.value;
+    console.log(amount);
+    for (let i = 0; i < amount; i += 1) {
+        total += 1;
+        console.log('Это тотал из функции счетчика', total);
+    }
+    return total;
+}
+const resertTotal = () => {
+    console.log('обнуляем тотал');
+    return total = 0;
+}
+
+// при клике на рендер создаем див, задаем рандомный цвет изменение размеров
+
 function getRandomColor(min, max){
   return Math.ceil(Math.random() * (max - min) + min)
 }
 function getWidth(width, count) {
-    console.log(count);
     return `${parseInt(width) + count*10}px`;
 }
 function getHeight(height, count) {
@@ -15,26 +42,44 @@ function getHeight(height, count) {
 }
 
 const createBoxes = (amount) => {
-    amount = valueEl.value - 1;
-    for (let i = 0; i <= amount; i += 1) {
-    
-            const itemEl = document.createElement('div');
-            itemEl.classList.add('item');
-            itemEl.style.backgroundColor = `rgb(${getRandomColor(0, 255)}, ${getRandomColor(0, 255)}, ${getRandomColor(0, 255)})`;
-            if (i === 0) {
-                itemEl.style.width = '30px';
-                itemEl.style.height = '30px';
-            } else {
-                itemEl.style.width = getWidth('30px',i);
-                itemEl.style.height = getHeight('30px',i);   
-            }
-            boxesEl.appendChild(itemEl);
+    amount = valueEl.value;
+    for (let i = 0; i < amount; i += 1) {
+        
+        const itemEl = document.createElement('div');
+        itemEl.classList.add('item');
+        itemEl.style.backgroundColor = `rgb(${getRandomColor(0, 255)}, ${getRandomColor(0, 255)}, ${getRandomColor(0, 255)})`;
+        if (i === 0 && counter.value === 0) {
+            itemEl.style.width = '30px';
+            itemEl.style.height = '30px'; 
+        } else if (counter.value > 0) {
+            itemEl.style.width = getWidth('30px', i+total);
+            itemEl.style.height = getHeight('30px', i+total);
+        } else {
+            itemEl.style.width = getWidth('30px', i);
+            itemEl.style.height = getHeight('30px', i);
+        }
+         boxesEl.appendChild(itemEl);
     }
 }
+
 renderBtn.addEventListener('click', createBoxes);
+renderBtn.addEventListener('click', countTotal);
+renderBtn.addEventListener('click', function () {
+    if (valueEl.value > 0) {
+        counter.increment();
+        console.log('это количество кликов на кнопку:', counter.value);
+    }
+});
+
+    // Очистка контейнера boxes по клику на кнопку
 
 const destroyBoxes = () => {
-    // console.log('удаляем все созданные дивы');
-    boxesEl.innerHTML = '';
-}
+        console.log('удаляем все созданные дивы');
+        boxesEl.innerHTML = '';
+    }
 destroyBtn.addEventListener('click', destroyBoxes);
+destroyBtn.addEventListener('click', function () {
+    counter.decrement();
+    console.log('обнуляем количество кликов');
+});
+destroyBtn.addEventListener('click', resertTotal);
